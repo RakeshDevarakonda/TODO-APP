0c9e28a241b50ece6d8fe20262c2bb6c6709e1ac
rakesh <rakeshdevarakonda2000@gmail.com>
2023-08-13 17:14:49 +0530

    added local storage

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let uolist = document.getElementById("uolist");
 let taskbox = document.getElementById("taskbox");
 let addbutton = document.getElementById("addbutton");
-let totaldiv=document.getElementById("totaldiv")
+let totaldiv = document.getElementById("totaldiv");
 
 taskbox.addEventListener("keypress", (e) => {
   if (e.key === "Enter" && taskbox.value != "") {
@@ -17,62 +17,19 @@ function addtask() {
   } else {
     let listitem = `
     <div class="totallistitem ">
-            <span class="spantag">${taskbox.value}</span>
-              <div class="editicon main">
+      <span class="spantag">${taskbox.value}</span>
+      <div class="editicon main">
               <i class="fa-solid fa-pen"></i>
-              </div>
-              <div class="deletebutton main">
+        </div>
+        <div class="deletebutton main">
               <i class="fa-solid fa-xmark"></i>
-              </div>
-           
-        </div>`;
+        </div>
+     </div>`;
 
     uolist.insertAdjacentHTML("afterbegin", listitem);
-
     taskbox.value = "";
-
-    let deletebutton = document.querySelectorAll(".deletebutton");
-    deletebutton.forEach((e) => {
-      e.addEventListener("click", () => {
-        e.parentNode.remove();
-  
-      });
-    });
-
-   
-
-    const editIcons = document.querySelectorAll(".editicon");
-
-    editIcons.forEach((editIcon) => {
-      editIcon.addEventListener("click", () => {
-        
-        const spanTag = editIcon.previousElementSibling.parentNode;
-        
-
-
-
-        
-           
-
-
-        document.body.addEventListener("click", (e) => {
-
-          if (e.target.parentNode.parentNode != totaldiv) {
-            spanTag.contentEditable = "true";
-            spanTag.focus()
-           
-          } else {
-            spanTag.contentEditable = "false";
-          }
-        });
-
-        spanTag.addEventListener("keypress", (e) => {
-          if (e.key === "Enter") {
-            spanTag.contentEditable = "false";
-          }
-        });
-      });
-    });
+    setlocal();
+    editbuttonanddelete();
   }
 }
 
@@ -81,3 +38,61 @@ addbutton.addEventListener("click", () => {
 });
 
 
+
+function editbuttonanddelete() {
+
+
+  let deletebutton = document.querySelectorAll(".deletebutton");
+  deletebutton.forEach((e) => {
+    e.addEventListener("click", () => {
+      e.parentNode.remove();
+      setlocal();
+    });
+  });
+
+
+
+  const editIcons = document.querySelectorAll(".editicon");
+
+  editIcons.forEach((editIcon) => {
+    editIcon.addEventListener("click", () => {
+
+      
+      const spanTag = editIcon.previousElementSibling;
+      let containertag = editIcon.previousElementSibling.parentNode;
+      console.log(containertag)
+
+      spanTag.contentEditable = "true";
+      containertag.focus()
+
+
+
+
+
+    
+
+      spanTag.addEventListener("keypress", (e) => {
+        if (e.key === "Enter") {
+          spanTag.contentEditable = "false";
+          setlocal();
+        }
+      });
+
+
+
+
+
+
+    });
+  });
+}
+
+function setlocal() {
+  localStorage.setItem("totaldata", uolist.innerHTML);
+}
+
+function loadlocal() {
+  uolist.innerHTML = localStorage.getItem("totaldata");
+  editbuttonanddelete();
+}
+loadlocal();
